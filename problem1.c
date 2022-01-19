9d464cc7b34cf6077753917ac148948a9c89a623
vijay pratap <vijayypratap002@gmail.com>
2022-01-19 22:48:26 +0530

    problem 1 solved

--- problem1.c
+++ problem1.c
@@ -1,1 +1,42 @@
-#include
+#include<stdio.h>
+
+int main(){
+int a=2,b=3,c=5;
+printf("table of %d\n",a);
+printf("%d\n",a*1);
+printf("%d\n",a*2);
+printf("%d\n",a*3);
+printf("%d\n",a*4);
+printf("%d\n",a*5);
+printf("%d\n",a*6);
+printf("%d\n",a*7);
+printf("%d\n",a*8);
+printf("%d\n",a*9);
+printf("%d\n",a*10);
+printf("\ntable of %d\n",b);
+printf("%d\n",b*1);
+printf("%d\n",b*2);
+printf("%d\n",b*3);
+printf("%d\n",b*4);
+printf("%d\n",b*5);
+printf("%d\n",b*6);
+printf("%d\n",b*7);
+printf("%d\n",b*8);
+printf("%d\n",b*9);
+printf("%d\n",b*10);
+printf("table of %d\n",c);
+printf("%d\n",c*1);
+printf("%d\n",c*2);
+printf("%d\n",c*3);
+printf("%d\n",c*4);
+printf("%d\n",c*5);
+printf("%d\n",c*6);
+printf("%d\n",c*7);
+printf("%d\n",c*8);
+printf("%d\n",c*9);
+printf("%d\n",c*10);
+
+
+printf("did not used FOR loop because haven't read about it yet.\n");
+return 0;
+}
